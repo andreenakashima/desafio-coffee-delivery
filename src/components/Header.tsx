22da9388fdcc4211370de2b2f15382e07860b5e6
andreenakashima <andreenakashima@gmail.com>
2022-11-07 14:40:42 -0300

    Criado header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,22 @@
+import { MapPin, ShoppingCart } from "phosphor-react";
+import Logo from "../assets/Logo.png";
+
 export function Header() {
-	return <div>Header</div>;
+	return (
+		<div className="flex items-center justify-between py-8">
+			<img src={Logo} alt="" />
+			<div className="flex gap-3">
+				<div className="flex gap-1 rounded-md bg-coffee-purple-light p-2 text-coffee-purple-dark">
+					<MapPin size={22} weight="fill" />
+					Porto Alegre, RS
+				</div>
+				<button className="relative rounded-md bg-coffee-yellow-light p-2 text-coffee-yellow-dark">
+					<ShoppingCart size={22} weight="fill" />
+					<span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-coffee-yellow-dark text-white ">
+						5
+					</span>
+				</button>
+			</div>
+		</div>
+	);
 }
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -4,8 +4,10 @@ import { Header } from "../components/Header";
 export function DefaultLayout() {
 	return (
 		<div className="bg-background h-screen w-full">
-			<Header />
-			<Outlet />
+			<div className="md:w-3/4 m-auto">
+				<Header />
+				<Outlet />
+			</div>
 		</div>
 	);
 }
